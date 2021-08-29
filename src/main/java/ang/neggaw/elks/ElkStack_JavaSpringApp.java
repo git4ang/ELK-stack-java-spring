771c03dae9960b78d60fbd099347a9b932281e3f
git4ang <code4ang@gmail.com>
2021-08-29 12:26:17 +0200

    Creation & implementation of logback-spring.xml (logs ELK)

--- src/main/java/ang/neggaw/elks/ElkStack_JavaSpringApp.java
+++ src/main/java/ang/neggaw/elks/ElkStack_JavaSpringApp.java
@@ -2,12 +2,12 @@ package ang.neggaw.elks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+@EnableElasticsearchRepositories(basePackages = "ang.neggaw.elks.repositories")
 @SpringBootApplication
 public class ElkStack_JavaSpringApp {
-
     public static void main(String[] args) {
         SpringApplication.run(ElkStack_JavaSpringApp.class, args);
     }
-
 }
